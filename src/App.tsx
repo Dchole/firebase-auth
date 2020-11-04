2f59e5eb4f8ca9c534afd9dcf0ab8f2f5b98d002
Dchole <derekoware47@gmail.com>
2020-11-04 20:45:00 +0000

    Firebase ui configuration and first render

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import firebase from "firebase/app";
+import * as firebaseui from "firebaseui";
+import "firebase/auth";
+import "firebase/firestore";
+import firebaseConfig from "./utils/firebase-config";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+firebase.initializeApp(firebaseConfig);
+
+const ui = new firebaseui.auth.AuthUI(firebase.auth());
+
+ui.start("#firebaseui-auth-container", {
+  signInOptions: [
+    {
+      provider: firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      signInMethod: firebase.auth.EmailAuthProvider.EMAIL_LINK_SIGN_IN_METHOD
+    },
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID
+  ]
+});
+
+const App = () => (
+  <main>
+    {/* The surrounding HTML is left untouched by FirebaseUI.
+  Your app may use that space for branding, controls and other customizations. */}
+    <h1>Welcome to My Awesome App</h1>
+    <div id="firebaseui-auth-container"></div>
+    <div id="loader">Loading...</div>
+  </main>
+);
 
 export default App;
---
+++ src/utils/firebase-config.ts
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAjP0dcrm6bYU98bybVyFimhjAhj2l9faY",
+  authDomain: "auth-ui-e12bc.firebaseapp.com",
+  databaseURL: "https://auth-ui-e12bc.firebaseio.com",
+  projectId: "auth-ui-e12bc",
+  storageBucket: "auth-ui-e12bc.appspot.com",
+  messagingSenderId: "371916702462",
+  appId: "1:371916702462:web:eafe0e5bf0b1831ededbaf"
+};
+
+export default firebaseConfig;
